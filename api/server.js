41c6da270871f73efe970d0ebab3ea80a66208a9
indiMjc <45151346+indiMjc@users.noreply.github.com>
2019-12-16 17:27:54 -0500

    authored usersRouter and set up endpoint to get all users

--- api/server.js
+++ api/server.js
@@ -2,6 +2,9 @@ const express = require('express');
 
 const server = express();
 
+const UsersRouter = require('../users/users-router');
+
 server.use(express.json());
+server.use('/users', UsersRouter);
 
 module.exports = server;
