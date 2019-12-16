7b2fd3168fe8b14828b249dfd681a527326414b3
indiMjc <45151346+indiMjc@users.noreply.github.com>
2019-12-16 16:25:32 -0500

    ran migration and seed files to make new "users" table with username and password properties

--- knexfile.js
+++ knexfile.js
@@ -5,7 +5,7 @@ module.exports = {
     client: 'sqlite3',
     useNullAsDefault: true,
     connection: {
-      filename: './data/<filename>.db3'
+      filename: './data/users.db3'
     },
     migrations: {
       directory: './data/migrations'
