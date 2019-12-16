2e98b37e867ce8e0c6ace7ebac339f9412499abd
indiMjc <45151346+indiMjc@users.noreply.github.com>
2019-12-16 16:51:55 -0500

    added environment variable and changed other port value in index.js

--- index.js
+++ index.js
@@ -2,8 +2,8 @@ require('dotenv').config();
 
 const server = require('./api/server');
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 5001;
 
-server.listen(PORT, (req, res) => {
+server.listen(PORT, () => {
   console.log(`\n***Server listening on port ${PORT}***\n`);
 });
