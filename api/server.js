d8430f37d07aadc96d8e5f21580c9c2c83f6a3bd
indiMjc <45151346+indiMjc@users.noreply.github.com>
2019-12-16 17:31:25 -0500

    register user works

--- api/server.js
+++ api/server.js
@@ -3,8 +3,10 @@ const express = require('express');
 const server = express();
 
 const UsersRouter = require('../users/users-router');
+const AuthRouter = require('../auth/auth-router');
 
 server.use(express.json());
 server.use('/users', UsersRouter);
+server.use('/auth', AuthRouter);
 
 module.exports = server;
