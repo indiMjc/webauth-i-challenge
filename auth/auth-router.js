ad3b5278b118cc7b6e93e444d929133c0c0e7e89
indiMjc <45151346+indiMjc@users.noreply.github.com>
2019-12-16 18:14:08 -0500

    login actually works now

--- auth/auth-router.js
+++ auth/auth-router.js
@@ -6,7 +6,7 @@ const Users = require('../users/users-model');
 router.post('/register', (req, res) => {
   let user = req.body;
 
-  const hash = bcrypt.hashSync('duckhasalotofgrease', 6);
+  const hash = bcrypt.hashSync(user.password, 6);
 
   user.password = hash;
 
@@ -26,12 +26,7 @@ router.post('/login', (req, res) => {
   Users.findBy({ username })
     .first()
     .then(user => {
-      console.log('async resolves to', bcrypt.compare(password, user.password));
-      console.log(
-        'sync resolves to',
-        bcrypt.compareSync(password, user.password)
-      );
-      user && bcrypt.compare(password, user.password)
+      user && bcrypt.compareSync(password, user.password)
         ? res.status(200).json({ message: `Welcome, ${user.username}!` })
         : res.status(401).json({ message: 'You shall not pass!' });
     })
