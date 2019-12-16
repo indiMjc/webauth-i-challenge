9ce24c1e1233468f19db53093e4079e3ad23ad16
indiMjc <45151346+indiMjc@users.noreply.github.com>
2019-12-16 17:21:43 -0500

    authored auth router

---
+++ auth/auth-router.js
@@ -0,0 +1,35 @@
+const bcrypt = require('bcryptjs');
+const router = require('express').Router();
+
+const Users = require('../users/users-model');
+
+router.post('/register', (req, res) => {
+  let user = req.body;
+
+  const hash = bcrypt.hashSync('duckhasalotofgrease', 6);
+
+  user.password = hash;
+
+  Users.add(user)
+    .then(saved => {
+      res.status(201).json(saved);
+    })
+    .catch(err => {
+      console.log(err);
+      res.status(500).json({ error: 'Internal error while registering user.' });
+    });
+});
+
+router.post('/login', (req, res) => {
+  let { username, password } = req.body;
+
+  Users.findBy({ username })
+    .first()
+    .then(user => {
+      user && bcrypt.compareSync(password, user.password)
+        ? res.status(200).json({ message: `Welcome, ${user.username}!` })
+        : res.status(401).json({ message: 'Invalid credentials.' });
+    });
+});
+
+module.exports = router;
