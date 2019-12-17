a478ffeeed79cbe3d3a04974d4e17e5b63cbad31
indiMjc <45151346+indiMjc@users.noreply.github.com>
2019-12-17 15:12:11 -0500

    imports for server.js and added use helmet on server

--- api/server.js
+++ api/server.js
@@ -1,10 +1,15 @@
 const express = require('express');
+const helmet = require('helmet');
+const sessions = require('express-session');
+const KnexSessionStore = require('connect-session-knex');
 
 const server = express();
 
 const UsersRouter = require('../users/users-router');
 const AuthRouter = require('../auth/auth-router');
+const knex = require('../data/dbConfig.js');
 
+server.use(helmet());
 server.use(express.json());
 server.use('/users', UsersRouter);
 server.use('/auth', AuthRouter);
